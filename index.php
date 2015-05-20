cabad3a6a07a98fafaa5b664de944d55242e9667
David Kim <hhjj0506@gmail.com>
2015-05-19 19:40:12 -0700

    added basics

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    //Make up the index page
+    require_once(__DIR__ . "/controller/login-verify.php");
+    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "/controller/create-db.php");
+    require_once(__DIR__ . "/view/footer.php");
+
+
---
+++ view/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="blog.css">
+        <title>Blog</title>
+    </head>
+    <body class="background">
+        <!--Makes the title-->
+        <div class="page-header">
+            <a href="http://localhost/kimd-blog/index.php"><h1>Blog</h1></a>
+            <a href="http://localhost:8383/Portfolio3DavidK/home.html"><button class="btn sm ghost" id="portfolio">Portfolio</button></a>
+        </div>
+        <!--Links to the pages-->
+        <a href="http://localhost/kimd-blog/login.php"><button class="btn md ghost" id="button">Login</button></a>
+        <a href="http://localhost/kimd-blog/controller/logout-user.php"><button class="btn md ghost" id="button">Logout</button></a>
+        <a href="http://localhost/kimd-blog/register.php"><button class="btn md ghost" id="button">Register</button></a>
+        
+                
